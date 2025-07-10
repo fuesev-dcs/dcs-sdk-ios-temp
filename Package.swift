83cca8f087d4d1ffe5da50615796923d204f2f40
Pipeline Agent <pipeline.agent@digitalchargingsolutions.com>
2025-07-10 13:03:21 +0000

    Update Package.swift for 0.1.5 [skip ci]

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // Automatically updated by pipeline.
-let sdkUrl = "https://github.com/fuesev-dcs/dcs-sdk-temp/releases/download/0.1.4/ChargeNowSDK.xcframework.zip"
-let checksum = "9efc7bf051dee3fe6d5974dba30d9ecebaa78ab4029ed3aee3cf87e1a929ce96"
+let sdkUrl = "https://github.com/fuesev-dcs/dcs-sdk-temp/releases/download/0.1.5/ChargeNowSDK.xcframework.zip"
+let checksum = "3b2913915ca9e44b3b6f518cfdc9e783a0fb9cbcde43902bdca96210ceaed543"
 
 let package = Package(
     name: "ChargeNowSDK",
